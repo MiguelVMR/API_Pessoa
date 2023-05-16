3421b89cdd445793d85dbead5e3fff319c616ce0
MiguelVilela <miguelmvilela12@gmail.com>
2023-05-16 16:04:32 -0300

    Adcionando migrations via flayway no projeto

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `addres`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Minas Gerais - Maria da Fé', 'Miguel', 'Male', 'Vilela'),
+	(2, 'Minas Gerais - Brasil', 'Beto', 'Male', 'Alberto'),
+	(4, 'Minas Gerais - Pouso Alegre - Brasil', 'Diego', 'Male', 'Santos Ribeiro'),
+	(5, 'Minas Gerais - Itajubá', 'João ', 'Male', 'Marcos'),
+	(7, 'Minas Gerais - Itajubá', 'Camily', 'Female', 'Silva'),
+	(9, 'Minas Gerais - Maria da Fé', 'Weskey', 'Male', 'Marciano'),
+	(10, 'Minas Gerais - Maria da Fé', 'Nikola', 'Male', 'Tesla');
