22930593db67d2897b83de0f295d361c0dcbbe04
MiguelVilela <miguelmvilela12@gmail.com>
2023-06-14 21:19:57 -0300

    Content negotiation feito para a requisição retornar JSON ou XML ou YAML

--- src/main/java/com/api/api_rest/controllers/PersonController.java
+++ src/main/java/com/api/api_rest/controllers/PersonController.java
@@ -2,7 +2,6 @@ package com.api.api_rest.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,46 +11,55 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.api.api_rest.data.vo.v1.PersonVO;
 import com.api.api_rest.services.PersonServices;
+import com.api.api_rest.util.MediaType;
 
 @RestController
 @RequestMapping("/api/person/v1")
 public class PersonController {
-
-    @Autowired
-    private PersonServices service;
-
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List <PersonVO> findAll()  {
-        return service.findAll();
-    }
-
-    @GetMapping(value = "/{id}",produces = 
-    MediaType.APPLICATION_JSON_VALUE)
-    public PersonVO findById(@PathVariable(value = "id") Long id) {
-        return service.findById(id);
-    }
-   
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity <?> delete (@PathVariable(value = "id") Long id) {
-        service.delete(id);
-        return ResponseEntity.noContent().build();
-    }
-   
-     @PostMapping(
-     produces = MediaType.APPLICATION_JSON_VALUE,
-     consumes = MediaType.APPLICATION_JSON_VALUE)
-    public PersonVO create(@RequestBody PersonVO person) {
-        return service.create(person);
-    }
-
-    @PutMapping(
-     produces = MediaType.APPLICATION_JSON_VALUE,
-     consumes = MediaType.APPLICATION_JSON_VALUE)
-    public PersonVO updade(@RequestBody PersonVO person) {
-        return service.update(person);
-    }
-
-    
+	
+	@Autowired
+	private PersonServices service;
+	
+	@GetMapping(produces = { MediaType.APPLICATION_JSON,
+			MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML 
+			})
+	public List<PersonVO> findAll() {
+		return service.findAll();
+	}
+	
+	@GetMapping(value = "/{id}",
+		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+				MediaType.APPLICATION_YML  })
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
+		return service.findById(id);
+	}
+	
+	@PostMapping(
+		consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+				MediaType.APPLICATION_YML  },
+		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+				MediaType.APPLICATION_YML  })
+	public PersonVO create(@RequestBody PersonVO person) {
+		return service.create(person);
+	}
+	
+	@PutMapping(
+		consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+				MediaType.APPLICATION_YML  },
+		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+				MediaType.APPLICATION_YML  })
+	public PersonVO update(@RequestBody PersonVO person) {
+		return service.update(person);
+	}
+	
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/api/api_rest/data/vo/v1/PersonVO.java
+++ src/main/java/com/api/api_rest/data/vo/v1/PersonVO.java
@@ -2,24 +2,22 @@ package com.api.api_rest.data.vo.v1;
 
 import java.io.Serializable;
 
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+@JsonPropertyOrder({"id", "first_name","last_name","addres","gender",})
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-   
-  
     private Long id;
-
-    
+    @JsonProperty("first_name")
     private String firstName;
-
-    
+    @JsonProperty("last_name")
     private String lastName;
 
-    
     private String addres;
-
-    
+    //@JsonIgnore
     private String gender;
 
     public PersonVO() {
@@ -115,4 +113,3 @@ public class PersonVO implements Serializable {
     }
 
 }
-
